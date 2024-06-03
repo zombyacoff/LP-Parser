1bae94c1a2ff1964ef4acf92c144b130320ccb56
zombyacoff <zombyacoff@gmail.com>
2024-06-03 16:16:15 +0300

    refactor

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import yaml
 
 LAUNCH_TIME = datetime.now()
 SEMAPHORE_MAX_LIMIT = 100
+# progress bar chars
+FULL_CHAR = "█"
+HALF_CHAR = "▒"
 
 
 def paint_text(text: str, color_code: int, bold=False) -> str:
@@ -19,9 +22,7 @@ def paint_text(text: str, color_code: int, bold=False) -> str:
 
 
 class Config:
-    def __init__(
-        self, config_path="config/settings.yml"
-) -> None:
+    def __init__(self, config_path="config/settings.yml") -> None:
         self.config_path = config_path
         self._load_settings()
         self._parse_settings()
@@ -39,42 +40,43 @@ class Config:
     def _parse_settings(self) -> None:
         try:
             self.offset_bool = self.config["offset"]["offset"]
-            self.offset_value = self.config["offset"]["value"] if self.offset_bool else 1
+            self.offset_value = (
+                self.config["offset"]["value"] if self.offset_bool else 1
+            )
             self.release_date_bool = self.config["release_date"]["release_date"]
             self.release_date = self.config["release_date"]["years"]
             self.websites_list = self.config["websites_list"]
             self.exceptions_list = self.config["exceptions_list"]
-            self.login_regex = self._2regex(self.config["for_advanced_users"]["login_regex"])
-            self.password_regex = self._2regex(self.config["for_advanced_users"]["password_regex"])
+            self.login_regex = self._compile_regex(
+                self.config["for_advanced_users"]["login_regex"]
+            )
+            self.password_regex = self._compile_regex(
+                self.config["for_advanced_users"]["password_regex"]
+            )
         except Exception as error:
             raise ValueError(f"The settings.yml file is incorrect: {error}")
 
-    def _2regex(self, regex_str: str) -> re.Pattern:
+    def _compile_regex(self, regex_str: str) -> re.Pattern:
         return re.compile(rf"{regex_str}")
 
     def _calculate_total_months(self) -> int:
-        if (self.release_date_bool
-            and len(self.release_date) == 1
-            and LAUNCH_TIME.year in self.release_date):
+        if self.release_date_bool and [LAUNCH_TIME.year] == self.release_date:
             return LAUNCH_TIME.month
         return 12
-    
+
     def _calculate_total_days(self) -> int:
         if self.total_months == 12:
-            return 366 # Number of days in a leap year
-        return sum(monthrange(2020, month)[1] 
-                   for month in range(1, self.total_months+1))
-    
+            return 366  # Number of days in a leap year
+        return sum(
+            monthrange(2020, month)[1] for month in range(1, self.total_months + 1)
+        )
+
     def _calculate_total_url(self) -> int:
-        return (len(self.websites_list) *
-                self.offset_value *
-                self.total_days)
+        return len(self.websites_list) * self.offset_value * self.total_days
 
 
 class OutputFile:
-    def __init__(
-        self, output_file_pattern: dict, folder_name="parser-output"
-) -> None:
+    def __init__(self, output_file_pattern: dict, folder_name="parser-output") -> None:
         self.folder_name = folder_name
         self.launch_time_format = LAUNCH_TIME.strftime("%d-%m-%Y-%H-%M-%S")
         self.output_file_name = f"{self.launch_time_format}.yml"
@@ -87,7 +89,7 @@ class OutputFile:
         os.makedirs(self.folder_name, exist_ok=True)
 
     def write_output(self, data: list[str]) -> None:
-        for i, key in enumerate(self.output_data):   
+        for i, key in enumerate(self.output_data):
             self.output_data[key][self.index] = data[i]
         self.index += 1
 
@@ -97,9 +99,7 @@ class OutputFile:
 
 
 class LPParser:
-    def __init__(
-        self, config: Config, output_file: OutputFile
-) -> None:
+    def __init__(self, config: Config, output_file: OutputFile) -> None:
         self.config = config
         self.output_file = output_file
         self.bar_counter = 1
@@ -107,25 +107,20 @@ class LPParser:
     def _get_progress_bar(self):
         percent = 100 * self.bar_counter / self.config.total_url
         bar_length = round(percent) // 2
-        bar = bar_length * "█" + (50-bar_length) * "▒"
-        print(
-            bar, paint_text(f"[{percent:.2f}%]", 1), 
-            f"[{self.bar_counter}/{self.config.total_url}]",
-            end="\r"
-        )
-        self.bar_counter += 1 
+        bar = bar_length * FULL_CHAR + (50 - bar_length) * HALF_CHAR
+        percent_progress = paint_text(f"[{percent:.2f}%]", 1)  # [14.43%]
+        progress_status = f"[{self.bar_counter}/{self.config.total_url}]"  # [210/366]
+        print(bar, percent_progress, progress_status, end="\r")
+        self.bar_counter += 1
 
-    async def _process_url(
-        self, url: str, session: aiohttp.ClientSession
-) -> None:
+    async def _process_url(self, url: str, session: aiohttp.ClientSession) -> None:
         try:
             async with session.get(url) as page:
                 self._get_progress_bar()
                 if page.status != 200:
                     return
                 soup = BeautifulSoup(await page.text(), "html.parser")
-            if (self.config.release_date_bool 
-                and not self._check_release_date(soup)):
+            if self.config.release_date_bool and not self._check_release_date(soup):
                 return
             self._parse(url, soup)
         except aiohttp.InvalidURL:
@@ -133,32 +128,33 @@ class LPParser:
 
     def _check_release_date(self, soup: BeautifulSoup) -> bool:
         time_element = soup.select_one("time")
-        release_date = (int(time_element.get_text("\n", strip=True)[-4:]) if time_element
-                         else LAUNCH_TIME.year)
+        release_date = (
+            int(time_element.get_text("\n", strip=True)[-4:])
+            if time_element
+            else LAUNCH_TIME.year
+        )
         return release_date in self.config.release_date
 
-    def _parse(
-        self, url: str, soup: BeautifulSoup
-) -> None:
+    def _parse(self, url: str, soup: BeautifulSoup) -> None:
         website_text = [sentence for sentence in soup.stripped_strings]
         credentials = self._extract_credentials(website_text) + (url,)
-        if credentials[0] != "": 
+        if credentials[0] != "":
             self.output_file.write_output(credentials)
 
-    def _extract_credentials(
-        self, website_text: list[str]
-) -> tuple[str, str]:
+    def _extract_credentials(self, website_text: list[str]) -> tuple[str, str]:
         login = password = ""
         for i, current in enumerate(website_text):
             email_match = self.config.login_regex.search(current)
-            if email_match and email_match.group() not in self.config.exceptions_list: 
+            if email_match and email_match.group() not in self.config.exceptions_list:
                 login = email_match.group()
                 if ":" in login:
                     data = login.split(":")
                     login, password = data[0], data[-1]
                     return login, password
                 for k in range(1, min(4, len(website_text) - i)):
-                    password_match = self.config.password_regex.search(website_text[i+k])
+                    password_match = self.config.password_regex.search(
+                        website_text[i + k]
+                    )
                     if password_match:
                         password = password_match.group()
                         return login, password
@@ -167,21 +163,30 @@ class LPParser:
     async def main(self) -> None:
         processes = []
         semaphore = asyncio.Semaphore(SEMAPHORE_MAX_LIMIT)
-        async with aiohttp.ClientSession() as session: 
-            for month in range(1, self.config.total_months+1):
-                for day in range(1, monthrange(2020, month)[1]+1):
+        async with aiohttp.ClientSession() as session:
+            for month in range(1, self.config.total_months + 1):
+                for day in range(1, monthrange(2020, month)[1] + 1):
                     for offset in range(self.config.offset_value):
-                        url_list = [f"{url}-{month:02}-{day:02}-{offset+1}" if offset > 0 
-                                    else f"{url}-{month:02}-{day:02}" 
-                                    for url in self.config.websites_list]
+                        url_list = [
+                            (
+                                f"{url}-{month:02}-{day:02}-{offset+1}"
+                                if offset > 0
+                                else f"{url}-{month:02}-{day:02}"
+                            )
+                            for url in self.config.websites_list
+                        ]
                         for url in url_list:
                             async with semaphore:
-                                process = asyncio.create_task(self._process_url(url, session))
-                                processes.append(process)         
-            print(f"""       
-Parsing has started...
-{paint_text("Do not turn off the program until the process is completed!", 31)}
-            """)
+                                process = asyncio.create_task(
+                                    self._process_url(url, session)
+                                )
+                                processes.append(process)
+            print(
+                f"\nParsing has started..."
+                f"\n{paint_text(
+                    "Do not turn off the program until the process is completed!", 
+                    31)}\n"
+            )
             await asyncio.gather(*processes)
         self.output_file.complete_output()
         elapsed_time = datetime.now() - LAUNCH_TIME
@@ -192,21 +197,17 @@ Parsing has started...
 
 
 def main():
+    # fmt: off
     print(paint_text(r"""
 _      ___         ___                               
 | |    | _ \  ___  | _ \  __ _   _ _   ___  ___   _ _ 
 | |__  |  _/ |___| |  _/ / _` | | '_| (_-< / -_) | '_|
 |____| |_|         |_|   \__,_| |_|   /__/ \___| |_|  
     """, 1))
+    # fmt: on
     try:
         config = Config()
-        output_file = OutputFile(
-            {
-                "login": {},
-                "password": {},
-                "url": {}
-            }
-        )
+        output_file = OutputFile({"login": {}, "password": {}, "url": {}})
         parser = LPParser(config, output_file)
         asyncio.run(parser.main())
     except ValueError as error:
