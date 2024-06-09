9f18c49cfeb79145edc1a94d5bd04a44f7942905
zombyacoff <zombyacoff@gmail.com>
2024-06-09 11:57:02 +0300

    refactor(exceptions)
    - base.py: in get_error_message method change Exception type to "ApplicationException"

--- src/exceptions/base.py
+++ src/exceptions/base.py
@@ -6,7 +6,7 @@ class ApplicationException(Exception):
     """Base class for all application exceptions"""
 
     @staticmethod
-    def get_error_message(exception: Exception) -> str:
+    def get_error_message(exception: "ApplicationException") -> str:
         print(
             ConsoleColor.paint_error(ERROR_TITLE),
             exception.message,
